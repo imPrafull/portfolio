84928527c82fffdf7adfcdd34029b7a846002034
prafull.sakpal <prafull.sakpal@i2econsulting.com>
2024-10-22 08:13:10 +0530

    project links added

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,22 +2,27 @@
 import classes from './ProjectCard.module.css'
 
 const ProjectCard = ({ project }) => {
-    return (
-      <a className={classes.card}>
-        <div className={classes['card-image-container']}>
-          {project.img ? (
-            <img src={project.img} alt={project.name} className={classes['card-image']} />
-          ) : (
-            <div className={classes['card-image']}>Image placeholder</div>
-          )}
-          <span className={classes.label}>{project.skills[0]}</span>
-        </div>
-        <div className={classes['card-content']}>
-          <h2 className={classes.headline}>{project.name}</h2>
-          <p className={classes.content}>{project.description}</p>
-        </div>
-      </a>
-    );
-  };
+
+  function openUrl(url) {
+    window.open(url, '_blank');
+  }
+
+  return (
+    <button className={classes.card} onClick={() => openUrl(project.demoLink)}>
+      <div className={classes['card-image-container']}>
+        {project.img ? (
+          <img src={project.img} alt={project.name} className={classes['card-image']} />
+        ) : (
+          <div className={classes['card-image']}>Image placeholder</div>
+        )}
+        <span className={classes.label}>{project.skills[0]}</span>
+      </div>
+      <div className={classes['card-content']}>
+        <h2 className={classes.headline}>{project.name}</h2>
+        <p className={classes.content}>{project.description}</p>
+      </div>
+    </button>
+  );
+};
   
-  export default ProjectCard;
+export default ProjectCard;
